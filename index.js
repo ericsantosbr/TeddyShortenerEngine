813ab37a9f7f84d885091a2a503b701634a5bd48
Eric Santos <ericsantosbr2@gmail.com>
2024-10-04 19:59:52 -0300

    Upload url to be shortened implemented

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const { generateUUID, generateShortURL } = require('./helpers/URLHelpers');
 const { config } = require('dotenv');
+const { retrieveShortenedURLTarget, uploadShortenedURL } = require('./helpers/DBhelpers');
 
 config();
 
 const app = express();
-app.listen(process.env.ENGINE_PORT || 8000);
+app.listen(process.env.ENGINE_PORT || 8355);
 
 app.post('/short/*', (req, res, next) => {
     const url = req.params[0];
 
     const entryUUID = generateUUID();
     const entryShortURL = generateShortURL();
+
+    const uploadData = {
+        shortenedurl: entryShortURL,
+        target: url
+    }
+
+    let uploadResult = uploadShortenedURL(uploadData);
 
     res.send('localhost/' + entryShortURL);
 
@@ -21,6 +29,20 @@ app.post('/short/*', (req, res, next) => {
 
 app.get('/', (req, res, next) => {
     res.send('Hello world!');
+
+    return next();
+});
+
+app.get('/:url', async (req, res, next) => {
+    const url = req.params.url;
+
+    const result = await retrieveShortenedURLTarget(url);
+
+    if (result.success) {
+        res.redirect(result.target);
+    } else {
+        res.send(result.message);
+    }
 
     return next();
 });
