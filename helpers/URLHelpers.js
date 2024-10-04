2b6537f3d0a2a2de443da5cc8e59060dfbbd71a6
Eric Santos <ericsantosbr2@gmail.com>
2024-10-04 19:59:11 -0300

    Modified naming

--- helpers/URLHelpers.js
+++ helpers/URLHelpers.js
@@ -11,11 +11,11 @@ function generateUUID() {
 
 function generateShortURL() {
     const characterLimit = process.env.ENGINE_SHORT_URL_CHARACTERS;
-    const engineAlphabet = process.env.ENGINE_ALPHABET;
+    const alphabet = process.env.ENGINE_ALPHABET;
     let outputURL = '';
 
     for (let i = 0; i < characterLimit; i++) {
-        outputURL = outputURL + engineAlphabet[Math.floor(Math.random() * (engineAlphabet.length - 1))];
+        outputURL = outputURL + alphabet[Math.floor(Math.random() * (alphabet.length - 1))];
     }
 
     console.log(outputURL);
