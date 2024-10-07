cba97ffb819c7289117776287c3af7f27bed34de
Eric Santos <ericsantosbr2@gmail.com>
2024-10-07 12:17:00 -0300

    Auth ruling changes

--- middlewawres/auth.js
+++ middlewawres/auth.js
@@ -17,15 +17,17 @@ async function validateUserAuth (req, res, next) {
             req.userId = bearerSearchResult.data.userId.user_id;
             return next();
         } else {
+            res.status(401);
             res.send('Invalid or expired token');
         }
     } else {
+        res.status(401);
         res.send('Authentication is required for this route');
     }
 }
 
 /**
- * Non-blocking function that catches authenticated user
+ * Non-blocking function that catches authenticated user. Only blocks is authentication fails
  * @param {*} req 
  * @param {*} res 
  * @param {*} next 
@@ -35,15 +37,21 @@ async function catchAuthenticatedUserData (req, res, next) {
         const authorizationBearer = req.headers.authorization.replace('Bearer ', '');
         let bearerSearchResult = await retrieveBearerData(authorizationBearer);
 
+        console.debug(authorizationBearer);
         console.debug(bearerSearchResult);
 
         if (bearerSearchResult.success && bearerSearchResult.data.isValid) {
             req.user = bearerSearchResult.data.userId.email;
             req.userId = bearerSearchResult.data.userId.user_id;
+
+            next();
+        } else {
+            res.status(401);
+            res.send('Authentication failed');
         }
+    } else {
+        next();
     }
-    
-    next();
 }
 
 module.exports = {
